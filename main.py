7d1826727271aed6fa2f32cf748f9b9e8c02746b
salihalperc <salihalpercetin@gmail.com>
2024-04-27 17:18:07 +0300

    correctness about edit post

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ def add_new_post():
 
 # Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-@admin_only
+
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
