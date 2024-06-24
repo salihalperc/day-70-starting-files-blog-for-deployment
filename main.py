b696b848323450c995ccc031d60f003c09e35923
salihalperc <salihalpercetin@gmail.com>
2024-06-24 21:10:28 +0300

    requirementstxt

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
